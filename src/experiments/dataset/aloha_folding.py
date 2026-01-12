9072f05c8ca298ed5f001de0273923982bf876a6
jstranghoener <jannick.stranghoener@uni-bielefeld.de>
2026-01-12 10:15:14 +0100

    bug fix in record.py, minor config changes

--- src/experiments/dataset/aloha_folding.py
+++ src/experiments/dataset/aloha_folding.py
@@ -6,14 +6,11 @@ from lerobot.share.configs import DatasetRecordConfig
 @dataclass
 @DatasetRecordConfig.register_subclass("aloha_folding")
 class AlohaFoldingDatasetConfig(DatasetRecordConfig):
-    repo_id: str = "hoodie_folding_v3/test2"
+    repo_id: str = "hoodie_folding_v3/hoodie_folding_eval"
     single_task: str = "Fold the hoodie"
-    root: str = "/media/nvme1/jstranghoener/lerobot/data/jannick-st/hoodie_folding_v3/test2"
-    num_episodes: int = 30
-    episode_time_s: int = 80.0
-    reset_time_s: int = 10.0
+    root: str = "/media/nvme1/jstranghoener/lerobot/data/jannick-st/hoodie_folding_v3/hoodie_folding_eval"
+    num_episodes: int = 50
+    episode_time_s: int = 90.0
+    reset_time_s: int = 15.0
     push_to_hub: bool = False
-    video_encoding_batch_size: int = 0  # write to disk every 30 episodes
-    num_image_writer_processes: int = 1
-    num_image_writer_threads_per_camera: int = 4
 
